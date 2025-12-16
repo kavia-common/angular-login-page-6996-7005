da8a93870ac5257fb0e2c90a181a69da34f11767
Kavia AI <production@kavia.ai>
2025-12-16 05:11:25 +0000

    Checkpoint 1 - cg3e4708f6

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/angular-login-page-6996-7005/angular_frontend
+npx eslint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
